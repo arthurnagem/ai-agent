ce8bb43ddb9af6d6f61145eef11e4e5216291721
“arthurnagem” <arthurnagem24@gmail.com>
2026-01-17 11:39:25 -0300

    enable LLM function calling

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 from google import genai
 import argparse
 from google.genai import types
+from prompts import system_prompt
+from functions.call_function import available_functions
 
 def main():
     load_dotenv()
@@ -24,8 +26,16 @@ def main():
 
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=messages
+        contents=messages,
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt,
+            tools=[available_functions],
+            temperature=0,
+        ),
     )
+
+    
+
     
     if response.usage_metadata is None:
         raise RuntimeError("Failed API request: usage metadata is missing.")
@@ -35,7 +45,11 @@ def main():
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     
-    print(response.text)
+    if response.function_calls:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(response.text)
 
 
 if __name__ == "__main__":
